1a7192b0faa95cec8252a69dd6527b4aa09ccfa9
Temitope <topephysics@gmail.com>
2020-01-14 10:25:16 +0100

    created animated card color validator

---
+++ src/components/AnimatedCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card"></div>
+</template>
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      validator: val => ['red', 'green', 'yellow', 'purple'].indexOf(val) !== -1,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.card {
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  height: 200px;
+  border-radius: 20px;
+  background-color: red;
+  transform: scale(1.2);
+}
+</style>
---
+++ src/components/CardDeck.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="deck">
+    <animated-card></animated-card>
+  </div>
+</template>
+<script>
+import AnimatedCard from './AnimatedCard.vue';
+
+export default {
+  components: {
+    AnimatedCard,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.deck {
+  position: relative;
+  width: 400px;
+  margin: auto;
+}
+</style>
